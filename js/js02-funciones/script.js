184592c70f1367bf60f3267618f859eee4fe86c6
Fernando Juarez <juarezsfw@outlook.com>
2023-11-13 17:25:24 -0600

    13-nov-2023 laptop funciones arrow, expresadas y declaradas

--- js/js02-funciones/script.js
+++ js/js02-funciones/script.js
@@ -1,3 +1,96 @@
 console.log("Sesion JS02 funciones ");
 
 
+
+// ---------------- Funcion declarada, funcion definidas ---------------
+// funcion declaration, function statement
+// Una de las caracteristica de las funciones declaradas es que tienen hosting pueden ser llamadas antes de su declaracion
+
+
+multiplica(5,3); // 15
+
+/**
+ * Multiplica dos números e imprime en consola
+ * @param {number} a valor de multplicando 
+ * @param {number} b valor de multiplicador
+ * 
+ */
+function multiplica( a,b ){
+    const resultado = a*b;
+    console.log(resultado);
+}
+
+function divide(dividendo,divisor) {
+    return dividendo/divisor;
+}
+
+console.log( divide(5,2) );  // 2.5
+console.log( divide(5,"2") );  // 2.5
+console.log( divide("5","2") );  // 2.5
+console.log( divide("5 0","2") );  // NaN
+
+
+// ---------------- Funcion expresadas ---------------
+// ( function expressions )
+/*
+Funciones que son declaradas dentro de la asignacion de una variable
+
+Estas funciones pueden ser anónimas (no tienen nombre)
+
+Las functiones expresadaas no tienen histing, porque no se carga en memoria hasta que se utilice
+*/
+
+// console.log( sum(6, 20)); // error cannot access sum before initialization
+
+const sum = function (a,b) {
+    return a + b;
+};
+
+console.log( sum(6, 20)); // 26
+
+const potencia = function( base,exponente ) {
+    return base**exponente;
+}
+
+console.log(potencia(2,3)); // 8;
+
+
+// ----------------- Funcion autoinvocadas ----------
+// (self-invoking functions)
+// se autoinvocan, no necesitan un llamado
+
+( function setUp(){
+    console.log("Me autoinvoco");
+    console.log("Puedo servirte como inicializador de tu programa");
+})();
+
+// --------------- Funcion flecha ------------
+//  ( arrow functions )
+/*
+Son similares a las funciones expresadas pero:
+
+- no requieren la palabra function
+- si tiene una sola instruccion no requiere llaves
+- si la instruccion es el mismo retorno no requiere la palabra return  
+
+*/
+
+// Area rectangulo con funcion expresada
+
+
+const areaRectangulo = function(length, width) {       
+    return length*width;
+}
+
+console.log(areaRectangulo(10,6));
+
+const areaRectanguloArrowFunction = (length, width) => length*width;
+
+console.log(areaRectanguloArrowFunction(3,2));
+
+
+const saludo = persona => `Hola ${persona}`;
+const ellaBailaSola = () => `Compa que le parece esa chica?`;
+
+console.log(saludo("Peso pluma"));
+
--- js/js02-funciones/var-let-const.js
+++ js/js02-funciones/var-let-const.js
@@ -42,3 +42,90 @@ function functionScope(){
     
 }
 functionScope();
+
+// ================ Block Scope ====================
+/*
+  Variables declaradas con let y const tienen un alcance de bloque. 
+  Esto significa que están limitadas al bloque en el que se declaran.
+*/
+{
+  var a = "a";
+  let b = "b";
+  const c = "c"; 
+
+  console.log(a);
+  console.log(b);
+  console.log(c);
+
+}
+
+console.log(a); // a
+// console.log(b); 
+// console.log(c);
+
+var personaA = "Natanael";
+let personaB = "Peso Pluma";
+const personaC = "Junior H";
+
+
+{
+  var personaA = "Margarita";
+  let personaB = "Angeles Azules";
+  const personaC = "Alacranes Musicales";
+
+  {
+    var personaA = "Tatiana";
+    let personaB = "La Granja de Zenón";
+    const personaC = "Cri-cri";
+
+    console.log(personaA); // Tatiana
+    console.log(personaB); // granja
+    console.log(personaC); // cri-cri
+  }
+}
+console.log(personaA); // Tatiana
+console.log(personaB); // Peso Pluma
+console.log(personaC); // Junior H
+
+
+
+
+// ================ Hoisting =======================
+/*
+  El "hoisting" en JavaScript es un comportamiento que ocurre durante 
+  la fase de compilación antes de la ejecución del código. 
+  Se refiere a la elevación (movimiento) de las declaraciones 
+  de variables y funciones al inicio de su contexto de ejecución
+
+*/
+
+
+/*
+  Las variables declaradas con var son elevadas al inicio de su contexto 
+  de ejecución (ya sea el ámbito de función o el ámbito global).
+  
+  Sin embargo, solo la declaración de la variable es elevada, 
+  no la asignación. Esto significa que la variable existe, 
+  pero su valor es undefined hasta que llega a la línea de asignación.
+*/
+
+console.log(phone); // undefined
+
+var phone;
+phone = "55-56-123-123";
+console.log(phone); // 55-56- ...
+
+/*
+  Las variables declaradas con let y const también se elevan al 
+  inicio de su contexto, pero a diferencia de var, no se inicializan 
+  automáticamente con undefined. La fase de inicialización de estas 
+  variables no ocurre hasta llegar a la línea de código donde se declaran.
+
+*/
+
+console.log( email ); // ReferenceError: Cannot access 'email' before initialization
+
+let email = "fernandoj@gmail.com";
+
+console.log( email );
+
