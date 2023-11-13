1bcdc32ed5ffd922ee34313922e50000190f4f8c
Fernando Juarez <juarezsfw@outlook.com>
2023-11-13 14:33:47 -0600

    13-nov-2023 PC let var const

---
+++ js/js02-funciones/script.js
@@ -0,0 +1,3 @@
+console.log("Sesion JS02 funciones ");
+
+
---
+++ js/js02-funciones/var-let-const.js
@@ -0,0 +1,44 @@
+console.log("Var vs let vs const");
+
+// ================ Reassigment and Redeclaration========
+/*
+ Declarar una variable significa que se reserva el nombre 
+ en memoria en el scope actual. 
+ 
+ Inicializar una variable es establecer el valor de la variable
+
+*/
+// Pueden ser reasignadas y redeclaradas dentro del 
+// mismo contexto de función.
+var userName = 10;
+userName = 20; // Reasignación válida
+var userName = 30; // Redeclaración válida en el mismo contexto de función
+
+// Pueden ser reasignadas dentro del mismo bloque y no pueden 
+// ser redeclaradas en el mismo ámbito.
+let age = 10;
+age = 20; // Reasignación válida
+// let age = 30; // Error: No se puede redeclarar en el mismo ámbito
+
+// No pueden ser reasignadas después de su inicialización y tampoco 
+// pueden ser redeclaradas en el mismo ámbito.
+const address = 10;
+// address = 20; // Error: No se puede reasignar
+// const address = 30; // Error: No se puede redeclarar
+
+/*
+  Variables declaradas con var, let y const tienen un alcance de función. 
+  Esto significa que están limitadas al contexto de la función 
+  en la que se declaran.
+*/
+function functionScope(){
+    var myVar = 10;
+    let myLet = 20;
+    const myConst = 30;
+
+    console.log(myVar);
+    console.log(myLet);
+    console.log(myConst);
+    
+}
+functionScope();
