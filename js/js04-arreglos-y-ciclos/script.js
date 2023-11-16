613393da58b8653c79dfdb903a5e5f56b833f05d
Fernando Juarez <juarezsfw@outlook.com>
2023-11-16 09:54:38 -0600

    15-nov-2023 lap  matrices y arrays

---
+++ js/js04-arreglos-y-ciclos/script.js
@@ -0,0 +1,18 @@
+console.log("Sesion JS04 arreglos y ciclos");
+
+/*
+   ========== Array ==========================
+   Es un tipo de estructura de datos que se utiliza
+   para almacenar una colección ordenada de elementos.
+   Estos elementos pueden ser de cualquier tipo.
+
+   Los arrays son objetos especiales con propiedades y
+   métodos específicos para trabajar con datos ordenados.
+
+*/
+
+// Declarar un arreglo usando corchetes 
+const nombresCh34 = ["Misael", "Luis", "Meli"];
+
+// Declarar un arreglo usando el constructor de la clase Array
+const apellidoCh34 = new Array("Navarro", "Ortega", "Flores");
