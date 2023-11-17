d0a891cc0c8ec805c45534b926df0c4869afe51d
Fernando Juarez <juarezsfw@outlook.com>
2023-11-17 13:07:07 -0600

    DOM

--- js/js05-condiciones-y-expresiones/script.js
+++ js/js05-condiciones-y-expresiones/script.js
@@ -131,8 +131,122 @@ console.log(y); // "2"
 /*
   === estrictamente igual, debe ser igual en el valor y el tipo
   !== estrictamente diferente, debe ser diferente el valor y tipo
-
 */
 
 console.log( "23" == 23  ); // true
 console.log( "23" === 23  ); // false 
+
+// ----------------- Operadores de comparación --------------
+/*
+  Comparar sus operandos  y devuelve un calor booleano.
+
+  < menor que
+  > mayor que
+  <= menor igual que
+  >= mayor igual que
+  == igual
+
+  Estos comparadores realizan una conversión implícita a numéricos
+  cuando los operandos son de diferente tipo de dato.
+
+  En el caso de que los dos operandos sean string, con los comparadores
+  <, >, >=, <= se compara caracter por caracter con su valor númerico
+  de la tabla unicode.
+
+  Valos en tabla unicode:
+   Números
+   Mayúsculas
+   Minúsculas
+
+*/
+
+console.log( 3 == "3" ); // true
+console.log( 3 == "tres" ); // false
+
+console.log( "Veinte" > "20" ); // true
+            // 86     >   50   posición del caracter en la tabla Unicode
+console.log( "Veinte" > 20 ); // false
+
+// ============= ejercicios ================
+
+
+console.log( true == "true"  );  // false
+         //    1  == NaN
+
+console.log(  23 < "40" );       // true
+
+console.log(  "b" < "A" );       // false 
+          //  98  <  65 
+
+console.log(  "Hola" < "HOLA");  // false
+          // 101(o)  <  79(O)        
+
+console.log(  "Hola" < "123");   // false
+          //   72(H) <  49(1)
+
+console.log(  "Hola" < "KeHace" + 1000 ); // true
+         //          9          11         prioridad    
+         //   "Hola" < "KeHace1000"  
+         //    72(H) <  75(K)   
+
+console.log(  "23" == 23  ); // true
+
+
+// --------------- Operadores lógicos AND (&&) y OR (||)
+
+const valueA = true, valueB = false, valueC = true;
+// const valueB = false;
+// const valueC = true;
+// const valueX = valueY = valueZ = true; <- Y y Z con declaradas con 'var'
+
+console.log( valueA || valueB && valueC ); // true
+          //        3          4        prioridad de operadores
+          // valueA ||     false   
+          //   true ||     false
+          //        true
+
+// Operadores de corto circuito
+/*
+  Cuando los operadores lógicos AND y OR actuan sobre operandos
+  diferentes a booleanos.
+ 
+  OP1 && OP2 -> Si OP1 es verdadero, se realiza la OP2 y este sería el resultado (OP2)
+  OP1 || OP2 -> Si OP1 es verdadero, el resultado es OP1, no se evalua OP2
+
+  Se considera falso: "", 0, NaN, null, undefined
+
+*/
+
+
+const varA = true;
+const varB = false;
+const varC = true;
+// OP1 && OP2    Si OP1 es verdadero, se ejecuta OP2
+console.log( varA && "Ya te la sabes" ); // Ya te la sabes
+console.log( varB && "Cámara, pivote y llanta" ); // false
+console.log( NaN && "Cámara, llanta y rin, mi Cheko"  ); // NaN
+console.log( "" && "Arre, carnal, gracias" ); // ""
+
+// OP1 || OP2    Si OP1 es verdadero, se ejecuta OP 1
+console.log( "false" || "Simona la mona" ); // "false"
+console.log( varA || "Ya te la sabes" ); // true
+console.log( varB || "Cámara, pivote y llanta" ); // "Cámara, pivote y llanta" 
+console.log( NaN  || "Cámara, llanta y rin, mi Cheko" ); // "Cámara, llanta y rin, mi Cheko"
+
+// Quiero imprimir  "Te la rifas" Si la
+// variable isOnline y isActive son true
+const isOnline = false;
+const isActive = true;
+
+if ( isOnline ){
+  if( isActive ){
+    console.log("Te la rifas ");
+  }
+}
+
+if ( isOnline && isActive ) console.log("Te la rifas");
+
+// Solución utilizando solo operados lógicos
+console.log(isOnline && isActive && "Te la rifas");
+
+isActive && isOnline && console.log("Te la rifas");
