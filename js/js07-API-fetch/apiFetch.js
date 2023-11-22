85769ad036f6df89fc21530ea0c9395d7588da8b
Fernando Juarez <juarezsfw@outlook.com>
2023-11-22 14:38:36 -0600

    23-nov-2023 PC asincrona, apifetch

---
+++ js/js07-API-fetch/apiFetch.js
@@ -0,0 +1,33 @@
+console.log("JS07 - apiFetch");
+
+/*
+ API: Application Programming Interface
+   Permite la comunicación entre dos aplicaciones de software
+   a travéz de un conjunto de reglas.
+
+ La API Fetch nos ayuda a acceder y manipular 
+ peticiones HTTP (DELETE, GET, POST, PUT).
+ 
+ Sintaxis:
+    fetch(resource, options)
+
+*/
+
+const urlFakeStore = "https://fakestoreapi.com/products";
+
+const getProducts = ( url ) => {
+    // Realizando solicitud Get
+    // .then() consume la promesa cuando sea resuelta
+    // .catch() se ejecuta en caso de que la promesa sea rechazada
+    fetch( urlFakeStore ).
+    then( (response) => {
+            console.log(response);
+            return response.json(); // promesa conversión de JSON a Object
+    })
+    .then( (products) => {
+        console.log(products);
+    } );
+
+};
+
+getProducts( urlFakeStore );
