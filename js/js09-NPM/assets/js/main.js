426bd6e803a6b649de21449a661c567da6d3f10c
Fernando Juarez <juarezsfw@outlook.com>
2023-11-24 17:38:57 -0600

    24-nov-2023 lap get y post con axios y fetch

--- js/js09-NPM/assets/js/main.js
+++ js/js09-NPM/assets/js/main.js
@@ -1,1 +1,66 @@
 console.log("JS09 - NPM");
+
+
+const url = "https://fakestoreapi.com/users";
+
+const getUsersUsingFetch = ( url ) => {
+    fetch(url)
+    .then( (respuesta) => {
+        return respuesta.json();
+    })
+    .then( (users) => {
+        console.log(users);
+    })
+    .catch( (error) => {
+        console.log(error);
+    });
+
+}
+
+getUsersUsingFetch( url );
+
+const getUsersUsingAxios = async ( url ) => {
+    try {
+     const users = await axios.get( url );
+     console.log( users.data );        
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+getUsersUsingAxios( url );
+
+const postUsingAxios = async (  ) =>{
+    const url = "https://reqres.in/api/users";
+    const user = {
+      name: "Aldo",
+      job: "Developer Ch35"
+    }
+ 
+    const response = await axios.post( url, user );
+    console.log( response.data );
+ }
+
+ postUsingAxios();
+
+ const postUsingFetch = async (  ) =>{
+    const url = "https://reqres.in/api/users";
+    const user = {
+      name: "Aldo",
+      job: "Developer Ch35"
+    }
+ 
+    const response = await fetch( url, {
+        method: "POST", // *GET, POST, PUT, DELETE, etc.
+        headers: {
+          "Content-Type": "application/json",
+ 
+        },
+        body: JSON.stringify(user), // body data type must match "Content-Type" header
+      });
+    const newUser = await response.json();
+    console.log( "Fetch" , newUser );
+ }
+
+
+ postUsingFetch();
