804bd15f42266023f5acdc388f98914260cace3e
Fernando Juarez <juarezsfw@outlook.com>
2023-12-01 14:45:53 -0600

    1-dic-2023 PC tipos de datos java

---
+++ java/eclipse/js03_tipos_de_datos/src/js03_tipos_de_datos/DataType.java
@@ -0,0 +1,153 @@
+package js03_tipos_de_datos;
+
+public class DataType {
+	public static void main(String[] args) {
+		/*
+         * Este es un comentario de bloque
+         * 
+         * Reglas para el nombre de las variables:
+         *  - Se distingue entre minúsculas y mayúsculas
+         *  - La longitud del nombre es ilimitada
+         *  - Puedes utilizar cualquier caracter Unicode
+         *  - La variable puede comenzar:
+         *    > una letra ej. miVariable
+         *    > signo de dolar $miVariable
+         *    > caracter de subrayado _miVariable
+         *  
+         */
+		
+		int $myVar;
+		int _numEngranes;
+		/*
+         * En Java tendremos  los siguientes tipo de variables:
+         *  Variables de instancia (Non static fields): atributos de los objetos.
+         *  Variables de clase (Static Fields): atributos de la clase.
+         *  Variables locales (local variables): variable dentro de las funciones(métodos)
+         *  Parámetros (parameters): Entrada de las funciones
+         * 
+         */
+		
+		/**
+	     *  https://docs.oracle.com/javase/tutorial/java/nutsandbolts/datatypes.html
+	     *  Tipos de datos en Java
+	     *  - Objetos
+	     *  - Datos primitivos
+	     *      8 datos primitivos
+	     *   > byte
+	     *   > shorte
+	     *   > int
+	     *   > long
+	     *   > float
+	     *   > double
+	     *   > boolean
+	     *   > char
+	     *      
+	     */
+		
+		// El tipo byte es de 8 bits, almacena datos numéricos enteros
+        byte myVarByte; // declarando la variable
+        myVarByte = 12; // asignarle un valor a la variable
+        /*
+         *  2^8 = 256
+         *  Para Obtener el valor máximo y mínimo del tipo byte
+         *  usando su wrapper (clase envolvete) Byte.
+         */
+        System.out.println("byte: Max value: " + Byte.MAX_VALUE ); // 127 -> 0... 127 = 128 valores
+        System.out.println("byte: Min value: " + Byte.MIN_VALUE ); // -128
+	
+        /*
+         *  Tipo de dato short
+         *  Dato de 16 bits, alamacena valores enteros
+         *  2^16 = 65536
+         *  Valores positivos:  0... 32767
+         *  Valores Negativos: -1... -32668
+         *  Obtener el MAX_value
+         *  
+         */
+	
+		short myShortValue;
+		System.out.println("short: Max value: " + Short.MAX_VALUE ); // 127 -> 0... 32767 = 128 valores
+	    System.out.println("short: Min value: " + Short.MIN_VALUE ); // -32768
+	    
+	    /*
+		 * Tipo de dato long
+		 * Dato de 64 bits, alamcena datos enteros
+		 * El wrapper de log es Long
+		 */
+		System.out.println("long Max Value: " + Long.MAX_VALUE ); // 9223372036854775807
+		System.out.println("long Min Value: " + Long.MIN_VALUE ); // -9223372036854775808
+		
+		/*
+		 * Las literales numéricas enteras en java son de tipo int
+		 * ej. 12, -456, 0
+		 * Para idicar una literal numética de tipo long
+		 * hay que indicar la literal con la letra l o L.
+		 * Se recomienda usar la L para no confundir con el 1
+		 */
+long edadPlaneta = 2_147_483_648L;
+		
+		// ==================================================================
+		/*
+		 * Dato de tipo float
+		 * Dato de 32 bits, almacena valos numéricos de punto flotante.
+		 * 
+		 * El wrapper de float es Float.
+		 * 
+		 */
+		System.out.println("float Max Value: " + Float.MAX_VALUE ); // 3.4028235E38
+		System.out.println("float Min Value: " + Float.MIN_VALUE ); // 1.4E-45
+		
+		/*
+		 * Dato de tipo double
+		 * Dato de 64 bits, almacena valos numéricos de punto flotante.
+		 * 
+		 * El wrapper de double es Double.
+		 * 
+		 */
+		System.out.println("double Max Value: " + Double.MAX_VALUE ); // 1.7976931348623157E308
+		System.out.println("double Min Value: " + Double.MIN_VALUE ); // 4.9E-324
+		
+		/*
+		 *  Las literales de punto flotante son por default double
+		 *  ej. 123.567, 0.012, -0.989
+		 */
+		float myVarFloat = 1500.6523F;
+		
+		/*
+		 * Dato Boolean
+		 * Almacea valores true o false
+		 */
+		boolean myVarBoolea = true;
+		
+		/*
+		 * Dato char
+		 * Almacena un solo caracter.
+		 * Se debe definir la literal con apóstrofe ej. 'a', 'C', ' '
+		 * Solo alamacea caracteres de la tabla unicode(16 bits).
+		 * puede alacenar del 0 al 65535
+		 */
+		
+		char primeraLetraDeNombre = 'S';
+		// Se puede alacenar por su valor unicode
+		char letraL = '\u004C'; //L
+		// También se puede definir por su valor numérico
+		char letraM = 77;
+		System.out.println("Letra L : " + letraL );
+		System.out.println("Letra M : " + letraM );
+		
+		/*
+		 * Formas de representar un número
+		 */
+		System.out.println("Representación decimal: " + 34 );
+		System.out.println("Representación hexadecimal: " + 0x22 );
+		System.out.println("Representación binaria: " + 0b0010_0010 );
+		System.out.println("Representación octal: " + 042 );
+		
+		/*
+		 *  Conversión de tipo de datos
+		 */
+		
+		
+	}
+
+}
--- js/js07-ejercicio/script-ejercicio.js
+++ js/js07-ejercicio/script-ejercicio.js
@@ -1,64 +1,86 @@
-console.log("a ver");
-
-
 // variable que almacena la url de la api 
 const url = "https://reqres.in/api/users?delay=5000";
-
+// variable que hace referencia al boton de HTML
 const startButton = document.getElementById("btn");
 
-// https://www.w3schools.com/jsref/met_element_addeventlistener.asp
-//startButton.addEventListener("click", (event) => {
-startButton.onclick = async (event) => {
-  // Evita el comportamiento predeterminado asociado a un evento.
-  event.preventDefault();
 
+startButton.onclick = async () => {
   const content = document.getElementById("products-container")
-
-  let localStorageUsarData = localStorage.getItem("userData");
-
+  const datosLocalStorage = localStorage.getItem("userData");
 
   const fechaNuevaSolicitud = new Date().getTime();
+  console.log(typeof (fechaNuevaSolicitud));
 
-  // checar si el local storage no esta vacio
-  if (localStorageUsarData !== null) {
+  // al momento de haber pulsado el boton, desactivarlo hasta que se termine de completar el bloque de codigo 
+  // de la condicion a la que entre
+  desactivarBoton(true);
+
+  // si hay algo de contenido en el local storage
+  if (datosLocalStorage !== null) {
+    // obtener la fecha en la que se realizo la anterior solicitud
     const fechaSolicitudPrevia = await obtenerFechaDelLocalStorage();
-    if (fechaSolicitudPrevia - fechaNuevaSolicitud > 6000) {
-      content.innerHTML = createSpinner();
-      let localStorageUsarData = localStorage.getItem("userData");
-      const users = JSON.parse(localStorageUsarData);
-      const infoParaMostrar = await mostrarEnDom(users);
+    /* si la diferencia entre el tiempo en que se almacenaron los datos y el tiempo en que se pulso el boton es 
+      mayor a un minuto (60000 milisegundos)
+      mostrar un spinner, borrar los datos del local storage,
+      volver a obtener los datos de la api, guardarlos en el local storage y mostrarlos en el DOM
+    */
+    if (fechaNuevaSolicitud - fechaSolicitudPrevia > 60000) {
+      content.innerHTML = showSpinner();
       await borrarDatosDelLocalStorage();
-      content.innerHTML = infoParaMostrar;
-
-    } else {
-      let localStorageUsarData = localStorage.getItem("userData");
-      const users = JSON.parse(localStorageUsarData);
+      await almacenarDatosEnLocalStorage(url);
+      const datosUsuariosLocalStorageJson = localStorage.getItem("userData");
+      const users = JSON.parse(datosUsuariosLocalStorageJson); // JSON.parse: transforma un string json en un objeto js
       const infoParaMostrar = await mostrarEnDom(users);
       content.innerHTML = infoParaMostrar;
+      desactivarBoton(false);
+      /*
+      de lo contrario solo mostrar los datos del local storage
+      */
+    } else {
+      const datosUsuariosLocalStorageJson = localStorage.getItem("userData");
+      const users = JSON.parse(datosUsuariosLocalStorageJson);
+      const infoParaMostrar = await mostrarEnDom(users);
+      content.innerHTML = infoParaMostrar;
+      desactivarBoton(false);
     }
+  /* si es la primera vez que se pulsa el boton, almacenar los datos en el local storage, 
+  volver a obtener los datos de la api, 
+  guardarlos en el local storage y mostrarlos en el DOM
+  */
   } else {
-    content.innerHTML = createSpinner();
-    const infoParaAlmacenar = await almacenarDatosEnLocalStorage(url)
-    let localStorageUsarData = localStorage.getItem("userData");
-    const users = JSON.parse(localStorageUsarData);
+    content.innerHTML = showSpinner();
+    await almacenarDatosEnLocalStorage(url);
+    const datosUsuariosLocalStorageJson = localStorage.getItem("userData");
+    const users = JSON.parse(datosUsuariosLocalStorageJson);
     const infoParaMostrar = await mostrarEnDom(users);
     content.innerHTML = infoParaMostrar;
+    desactivarBoton(false);
   }
 
 };
 
+
+
+/**
+ * se crea una promesa para obtener la informacion de la url y se consume la promesa,
+ * se toma la respuesta de dicha promesa y se lee hasta el final (response.json()) creando así una nueva promesa,
+ * la segunda promesa es consumida y se obtiene un objeto JS. De dicho objeto se toma solo el valor del atributo data y se almacena dicha infromación en formato JSON en el localStorage 
+ *también se guarda la fecha de creación de dicho objeto en el localStorage
+ * 
+ * @param {String} url 
+ * 
+ */
 const almacenarDatosEnLocalStorage = async url => {
-  fetch(url)
+  await fetch(url)
     .then((response) => {
       return response.json();
     })
     .then((users) => {
       const arreglo = users.data;
-      console.log(arreglo);
 
       const infoJsonLocalStorage = localStorage.setItem("userData", JSON.stringify(arreglo));
       const fechaSolicitudOriginal = localStorage.setItem("fechaSolicitud", JSON.stringify(new Date().getTime()));
-      console.log("si llego a almacenar datos y la fecha");
+
     })
     .catch((error) => {
       console.log(error);
@@ -66,16 +88,21 @@ const almacenarDatosEnLocalStorage = async url => {
 
 }
 
-// funcion para mostrar los datos de un hecho string
-function mostrarEnDom(datos) {
-
-  const arregloObjeto = datos;
-  console.log(arregloObjeto);
+/**
+ * Itera sobre un arreglo de objetos, muestra 
+ * los valores de los atributos:
+ * id, email, first_name, last_name y avatar
+ * en una tabla de bootstrap 
+ * y retorna la tabla con dichos datos
+ * @param {Object[]} arreglo 
+ * @returns {String}
+ */
+ const mostrarEnDom = async (arreglo) => {
   // con el metodo map nos da un nuevo arreglo,
   // forEach modifica el arreglo original
   // https://getbootstrap.com/docs/5.3/content/tables/
-  const personas = arregloObjeto.map((element, index, array) => `
-  <table class="table"> 
+  const personas = arreglo.map((element, index, array) => `
+  <table class="table table-dark"> 
   <thead>
     <tr>
       <th scope="col">#</th>
@@ -88,33 +115,41 @@ function mostrarEnDom(datos) {
   <tbody>
     <tr>
       <th scope="row">${element.id}</th>
-        <td>${element.email}</td>
-        <td>${element.first_name}</td>
-        <td>${element.last_name}</td>
-      <td><img src="${element.avatar}" alt="avatar"/></td>
+        <td class="text-white">${element.email}</td>
+        <td class="text-white">${element.first_name}</td>
+        <td class="text-white">${element.last_name}</td>
+      <td class="text-white"><img src="${element.avatar}" class="rounded-circle" alt="avatar"/></td>
     </tr>
   </tbody>
   </table>
   `);
-
-  console.log(personas);
   return personas;
 
 }
 
-
-async function obtenerFechaDelLocalStorage() {
+/**
+ * Obtiene la fecha almacenada en el local storage
+ * @returns {Date().getTime()}
+ */
+const obtenerFechaDelLocalStorage = async () => {
   const fechaSolicitud = localStorage.getItem("fechaSolicitud");
   const fecha = JSON.parse(fechaSolicitud);
   return fecha;
 }
 
-async function borrarDatosDelLocalStorage() {
+/**
+ * Borra el string JSON de la informacion de los usuarios y la fecha almacenados en el local storage por la función almacenarDatosEnLocalStorage
+ */
+const borrarDatosDelLocalStorage = async() => {
   localStorage.removeItem("userData");
   localStorage.removeItem("fechaSolicitud");
 }
 
-const createSpinner = () => {
+/**
+ * Crea un spinner con estilo bootstrap
+ * @returns {String}
+ */
+const showSpinner = () => {
   const spinner = `
     <div class="spinner-border text-primary" role="status">
       <span class="visually-hidden">Loading...</span>
@@ -123,7 +158,10 @@ const createSpinner = () => {
   return spinner;
 }
 
-const disableStartButton = (valor) => {
+/**
+ * Deshabilita el botón del HTML si el argumento es true. Habilita el botón si el argumento es false.
+ * @param {Boolean} valor 
+ */
+const desactivarBoton = (valor) => {
   startButton.disabled = valor;
 }
-
